4ffafe42d3df9cb6ee02be61b55773a342e9e6d0
Ryo-Ura <urawaryosuke@gmail.com>
2022-03-24 16:51:56 -0700

    a

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = new Date().getFullYear() - birthYear;
+    console.log(firstName + ' is ' + age + ' years old.');
+    return age;
+}
+
+const firstName = 'John';
+calcAge(1991);
